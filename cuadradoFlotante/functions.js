5c45e24bfa792df3d1f9af5cace7a3531832d2d5
Cesar Alarcon <ciam042@gmail.com>
2018-10-23 21:35:27 -0500

    modificacion cuadrado flotante

--- cuadradoFlotante/functions.js
+++ cuadradoFlotante/functions.js
@@ -1,28 +1,17 @@
-var boxElement = document.getElementById('movingBox');
-        var move=false;
-        var speed = 10, // the box will move by 10 pixels on every step
-        direction = 1; // 1 moves in the positive direction; -1 vice versa
-        function moveDiv(){
-            move=!move;
-            console.log("move= "+move);
-            while(move){
-                // Calculate and store some of the box coordinates:
-                var boxLeftPos = boxElement.offsetLeft,
-                    boxRightPos = boxLeftPos + boxElement.offsetWidth;
-                // When right side of the box goes too far - change direction:
-                if (boxRightPos > document.body.offsetWidth) {
-                    direction = -1;
-                }
-                // When left side of the box goes too far - change direction:
-                if (boxLeftPos < 0) {
-                    direction = 1;
-                }
-                // Recalculate position:
-                boxElement.style.left = (boxLeftPos + speed * direction) + 'px';
-            
+var parent = document.getElementsByTagName('section')[0];
+if(parent.hasChildNodes()){
+    console.log("yes");
+    var movingBox= parent.firstChild;
+    console.log(parent.firstChild.innerHTML);
+    movingBox.style.position="relative";
+    movingBox.style.left="100px;"
+    movingBox.innerHtml="MovingBox";
+    
+} else{
+    console.log("nope");
+}  
         
-             }
-        }
+        
 
         
         
